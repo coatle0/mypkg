7bcf93472b7c9e1656423ce089176506716ec05c
coatle0 <coatle0@gmail.com>
2026-04-08 11:10:10 +0900

    add initi_volmon_ori

--- R/myfunc.R
+++ R/myfunc.R
@@ -1439,6 +1439,73 @@ update_myuidx<-function(qtr_ref_date,week_ref_date,idx_fn){
   reorder_gs_sheet('us_idx',sector_rank)
 }
 
+init_volmon_ori<-function(wd){
+  vmonenv <- new.env()
+  diff_env <- new.env()
+  nor_vol_env <- new.env()
+  
+  wd_str<-paste0("C:/",wd,"/")
+  
+  assign("wd_str",wd_str,envir=.GlobalEnv)
+  
+  today_str <- Sys.Date()
+  assign('today_str',today_str,envir=.GlobalEnv)
+  
+  code<-code_get()
+  assign('code',code,envir=.GlobalEnv)
+  
+  time_index <- as.POSIXct(character())
+  empty_xts <- xts(matrix(ncol = 5, nrow = 0), order.by = time_index)
+  
+  # 컬럼 이름 지정
+  colnames(empty_xts) <- c('open','high','low','close','volume')
+  
+  
+  volmon <- read_csv('c:/lab/s101_v2.csv',locale=locale('ko',encoding='cp949'))
+  
+  jm_lst<-substr(volmon$jm,2,7)
+  assign('jm_lst',jm_lst,envir=.GlobalEnv)
+  
+  idx_gs_lst <- read_gs_idx('kr_idx')
+  kweight_lst<-idx_gs_lst[[2]]
+  ksmb_lst<-idx_gs_lst[[1]]
+  assign('ksmb_lst',ksmb_lst,envir=.GlobalEnv)
+  assign('kweight_lst',kweight_lst,envir=.GlobalEnv)
+  
+  
+  
+  jmnm_lst<-lapply(jm_lst,function(x) code$name[match(x,code$scode)])
+  jmnm_lst <- unlist(jmnm_lst)
+  assign('jmnm_lst',jmnm_lst,envir=.GlobalEnv)
+  
+  init_envir<-lapply(jmnm_lst,function(x) assign(x,empty_xts,envir=vmonenv))
+
+  #load vsd file
+  # vsdfm_lst<-lapply(ksmb_lst,function(x){
+  #   dut<-read.csv(paste0(wd_str,'/tm_study/',x,'_vsdfm.csv'));
+  #   dt<-paste0(today_str,' ',dut$idx)
+  #   dt<-as.POSIXct(dt,format="%Y-%m-%d %H:%M");
+  #   #print(y)
+  #   dut_x <- xts(dut[,c('mean','sd3')],dt);
+  #   assign(paste0(code[match(substr(x,2,7),code$code),2]$name,"_vsdfm"),dut_x,envir=vmonenv)
+  # }
+  
+  assign('vmonenv',vmonenv,envir=.GlobalEnv)
+  assign('diff_env',diff_env,envir=.GlobalEnv)
+  assign('nor_vol_env',nor_vol_env,envir=.GlobalEnv)
+
+  #initialize tgt_name_sort and assign as global variable
+  tgt_name_sort <- data.frame()
+  assign('tgt_name_sort',tgt_name_sort,envir=.GlobalEnv)
+
+
+  save.image(file=paste0(wd_str,"vmonGlobal.RData"))
+  #saveRDS(vmonenv,file=paste0(wd_str,"vmonenv.RData"))
+  #saveRDS(diff_env,file=paste0(wd_str,"diff_env.RData"))
+  #saveRDS(nor_vol_env,file=paste0(wd_str,"nor_vol_env.RData"))
+  
+}
+
 
 init_volmon<-function(wd){
   vmonenv <- new.env()
